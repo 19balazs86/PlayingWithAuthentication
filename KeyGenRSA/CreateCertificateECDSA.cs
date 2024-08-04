defb83e2e8ee98578fb8940b83471b2ee362a1ba
Balazs Kereszturi <19balazs86@gmail.com>
2024-08-04 17:50:14 +0200

    Add CreateCertificateECDSA

---
+++ KeyGenRSA/CreateCertificateECDSA.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace KeyGenRSA;
+
+public static class CreateCertificateECDSA
+{
+    public static async Task DoIt()
+    {
+        using ECDsa ecdsa = ECDsa.Create(ECCurve.NamedCurves.nistP256);
+
+        string privateKeyPem = ecdsa.ExportECPrivateKeyPem();
+        string publicKeyPem  = ecdsa.ExportSubjectPublicKeyInfoPem();
+
+        var certificateRequest = new CertificateRequest("CN=My-SelfSigned-Cert", ecdsa, HashAlgorithmName.SHA256);
+
+        using X509Certificate2 certificate = certificateRequest.CreateSelfSigned(DateTimeOffset.Now, DateTimeOffset.Now.AddYears(1));
+
+        string certificateText = exportAsCertText(certificate);
+
+        await File.WriteAllTextAsync("Key-ECDsa-Private.key", privateKeyPem);
+        await File.WriteAllTextAsync("Key-ECDsa-Public.key",  publicKeyPem);
+        await File.WriteAllTextAsync("Key-ECDsa-Cert.crt",    certificateText);
+
+        // X509Certificate2 certificateFromFile = X509Certificate2.CreateFromPemFile("Key-ECDsa-Cert.crt", "Key-ECDsa-Private.key");
+    }
+
+    private static string exportAsCertText(X509Certificate2 certificate)
+    {
+        byte[] certificateBytes = certificate.Export(X509ContentType.Cert, "password");
+
+        var stringBuilder = new StringBuilder();
+
+        stringBuilder.AppendLine("-----BEGIN CERTIFICATE-----");
+        stringBuilder.AppendLine(Convert.ToBase64String(certificateBytes, Base64FormattingOptions.InsertLineBreaks));
+        stringBuilder.AppendLine("-----END CERTIFICATE-----");
+
+        return stringBuilder.ToString();
+    }
+}
--- KeyGenRSA/Program.cs
+++ KeyGenRSA/Program.cs
@@ -1,13 +1,23 @@
 ﻿using System.Security.Cryptography;
 
-using RSA keyRSA = RSA.Create();
+namespace KeyGenRSA;
 
-//byte[] privateKeyBytes = keyRSA.ExportRSAPrivateKey();
-//await File.WriteAllBytesAsync("KeyPrivateRSA", privateKeyBytes);
-//keyRSA.ImportRSAPrivateKey(privateKeyBytes, out _);
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        using RSA keyRSA = RSA.Create();
 
-string privateKeyPem = keyRSA.ExportRSAPrivateKeyPem();
-string publicKeyPem  = keyRSA.ExportRSAPublicKeyPem();
+        //byte[] privateKeyBytes = keyRSA.ExportRSAPrivateKey();
+        //await File.WriteAllBytesAsync("KeyPrivateRSA", privateKeyBytes);
+        //keyRSA.ImportRSAPrivateKey(privateKeyBytes, out _);
 
-await File.WriteAllTextAsync("Key-RSA-Private.pem", privateKeyPem);
-await File.WriteAllTextAsync("Key-RSA-Public.pem",  publicKeyPem);
+        string privateKeyPem = keyRSA.ExportRSAPrivateKeyPem();
+        string publicKeyPem  = keyRSA.ExportRSAPublicKeyPem();
+
+        await File.WriteAllTextAsync("Key-RSA-Private.pem", privateKeyPem);
+        await File.WriteAllTextAsync("Key-RSA-Public.pem",  publicKeyPem);
+
+        // await CreateCertificateECDSA.DoIt();
+    }
+}
