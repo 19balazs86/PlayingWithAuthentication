bd47fcd6acbd452f7fb75de2abac8695192f591d
Balazs Kereszturi <19balazs86@gmail.com>
2024-08-16 14:53:20 +0200

    Small changes in: Hashing_PBKDF2

--- KeyGenRSA/Hashing_PBKDF2.cs
+++ KeyGenRSA/Hashing_PBKDF2.cs
@@ -4,16 +4,20 @@ namespace KeyGenRSA;
 
 public static class Hashing_PBKDF2
 {
-    private const int _keySize    = 64;
-    private const int _iterations = 350000;
+    private const int _saltSize   = 32;
+    private const int _hashSize   = 64;
+    private const int _iterations = 350_000;
 
     private static readonly HashAlgorithmName _hashAlgorithmName = HashAlgorithmName.SHA512;
 
     public static string HashPasword(ReadOnlySpan<char> password, out ReadOnlySpan<byte> salt)
     {
-        salt = RandomNumberGenerator.GetBytes(_keySize);
+        salt = RandomNumberGenerator.GetBytes(_saltSize);
 
-        byte[] passwordHash = Rfc2898DeriveBytes.Pbkdf2(password, salt, _iterations, _hashAlgorithmName, _keySize);
+        byte[] passwordHash = Rfc2898DeriveBytes.Pbkdf2(password, salt, _iterations, _hashAlgorithmName, _hashSize);
+
+        // Return value could be: Hash-Salt, which you can store in 1 field
+        // return $"{Convert.ToHexString(passwordHash)}-{Convert.ToHexString(salt)}";
 
         return Convert.ToHexString(passwordHash);
     }
@@ -27,11 +31,13 @@ public static class Hashing_PBKDF2
 
     public static bool VerifyPassword(ReadOnlySpan<char> password, ReadOnlySpan<char> passwordHash, ReadOnlySpan<byte> salt)
     {
-        byte[] hashToCompare = Rfc2898DeriveBytes.Pbkdf2(password, salt, _iterations, _hashAlgorithmName, _keySize);
+        byte[] hashToCompare = Rfc2898DeriveBytes.Pbkdf2(password, salt, _iterations, _hashAlgorithmName, _hashSize);
 
         byte[] hashBytes = Convert.FromHexString(passwordHash);
 
-        return hashToCompare.SequenceEqual(hashBytes);
+        // return hashToCompare.SequenceEqual(hashBytes);
+
+        return CryptographicOperations.FixedTimeEquals(hashToCompare, hashBytes); // More secure
     }
 
     public static bool TestHashing(ReadOnlySpan<char> password)
